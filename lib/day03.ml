4eec1a17a0ae2bb219bf1a2d1dd4973ac21e3f52
Damien Ragoucy <damien.ragoucy@mckay-brothers.com>
2026-03-05 16:19:27 +0100

    Solve day 3 part 2

--- lib/day03.ml
+++ lib/day03.ml
@@ -46,3 +46,52 @@ let solve_part_1 (input: input) (* : int *) =
 let _ = solve_part_1 sample_input
 let _ = solve_part_1 input
 *)
+
+(* Part 2 *)
+
+let split_list_at ~i ~list =
+  let rec accumulate head cnt tail =
+    match tail with
+    | [] -> failwith "length shorter than index"
+    | x :: tail' when cnt = i -> (List.rev @@ x :: head, tail')
+    | x :: tail' -> accumulate (x :: head) (cnt + 1) tail'
+  in
+  accumulate [] 0 list
+
+(* let _ = split_list_at ~i:3 ~list:(Int.range 10 30 |> List.of_iter) *)
+
+let format_joltage batteries =
+  batteries
+  |> List.map ~f:Int.to_string
+  |> String.concat ~sep:""
+  |> Int.of_string_exn
+
+let get_joltage_12 (line: int list) =
+  let acc, tail = split_list_at ~i:(12 - 1) ~list:line in
+  let keep_best_batteries acc i =
+    let acc' = acc @ [i] in
+    let indices = Int.range 0 (List.length acc' - 1) |> List.of_iter in
+    List.fold_left indices ~init:[]
+      ~f:(fun numbers index ->
+          let batteries = List.remove_at_idx index acc' in
+          let number = format_joltage batteries in
+          (batteries, number) :: numbers)
+    |> List.sort ~cmp:(fun x y ->
+        let _, n = x in
+        let _', n' = y in
+        Int.compare n' n)
+    |> List.get_at_idx_exn 0
+    |> (fun (batteries, _) -> batteries)
+  in
+  List.fold_left tail ~init:acc ~f:keep_best_batteries
+
+(*
+let _ = input |> List.get_at_idx_exn 0 |> get_joltage_12
+
+Result
+let _ =
+  input
+  |> List.map ~f:get_joltage_12
+  |> List.fold_left ~init:0
+    ~f:(fun acc batteries -> format_joltage batteries + acc)
+*)
